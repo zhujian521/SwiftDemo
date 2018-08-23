1db9b14a186a6a053f70a33ca224560c13a056b2
zhujian521 <1179758791@qq.com>
2018-08-23 22:20:08 +0800

    add 注释

--- FirstProject/AppDelegate.swift
+++ FirstProject/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow.init(frame: UIScreen.main.bounds)
-//        let readingString = "reading"
+//        add message
         var dic:[NSString:AnyObject] =  String.readingData() as [NSString : AnyObject]
         let type:String = dic["type"] as! String
         if type.isEmpty {
